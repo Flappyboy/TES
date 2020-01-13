d7cfeb558678de29050578a53b0b67d940efad5f
jachly <jachly@qq.com>
2020-01-13 10:14:41 +0800

    Dts添加相关的sha

--- plugin-jhkt/src/main/java/top/jach/tes/plugin/jhkt/dts/Dts.java
+++ plugin-jhkt/src/main/java/top/jach/tes/plugin/jhkt/dts/Dts.java
@@ -2,6 +2,28 @@ package top.jach.tes.plugin.jhkt.dts;
 
 import top.jach.tes.plugin.tes.code.bug.Bug;
 
+import java.util.*;
+
 public class Dts extends Bug {
 
+    // Key->RepoName, Value->shas
+    private Map<String, Set<String>> repoShasMap = new HashMap<>();
+
+    public Dts addShas(String repoName, String... newShas){
+        Set<String> shas = repoShasMap.get(repoName);
+        if(shas == null){
+            shas = new HashSet<>();
+        }
+        shas.addAll(Arrays.asList(newShas));
+        return this;
+    }
+
+    public Map<String, Set<String>> getRepoShasMap() {
+        return repoShasMap;
+    }
+
+    public Dts setRepoShasMap(Map<String, Set<String>> repoShasMap) {
+        this.repoShasMap = repoShasMap;
+        return this;
+    }
 }
