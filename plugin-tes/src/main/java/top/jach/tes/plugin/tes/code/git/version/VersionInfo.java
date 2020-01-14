dada6806638959ccef60ec348e17142574f1e135
jachly <jachly@qq.com>
2020-01-14 10:05:08 +0800

    VersionInfo

---
+++ plugin-tes/src/main/java/top/jach/tes/plugin/tes/code/git/version/VersionInfo.java
@@ -0,0 +1,37 @@
+package top.jach.tes.plugin.tes.code.git.version;
+
+import top.jach.tes.core.api.domain.info.Info;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VersionInfo extends Info {
+    public static final String INFO_NAME = "VersionInfo";
+    private Long reposId;
+    private Version version;
+
+    public static VersionInfo createInfo(Version version){
+        VersionInfo info = new VersionInfo();
+        info.initBuild();
+        info.setVersion(version);
+        return info;
+    }
+
+    public Long getReposId() {
+        return reposId;
+    }
+
+    public VersionInfo setReposId(Long reposId) {
+        this.reposId = reposId;
+        return this;
+    }
+
+    public Version getVersion() {
+        return version;
+    }
+
+    public VersionInfo setVersion(Version version) {
+        this.version = version;
+        return this;
+    }
+}
